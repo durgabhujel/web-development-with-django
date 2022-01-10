a3fe3c6df41d1bf9f2d28f4b2b3fd9ff54b4c423
Deepen2056 <deepen916@gmail.com>
2022-01-10 16:28:13 +0545

    added builtin file

---
+++ python-crash-course/Function-modules/builtin.py
@@ -0,0 +1,30 @@
+# Built in modules
+#os
+#sys
+#math
+#datetime
+#random
+import calculator
+import os 
+import math as m
+# r = os.getcwd()
+# a = 'newfolder'
+# os.mkdir(a)
+# print(r)
+
+
+
+# f5 = math.factorial(5)
+# print(f5)
+
+# result = dir(calculator)
+# print(result)
+
+# print(calculator.a)
+
+# r = dir(m)
+# print(r)
+# print(type(r))
+
+#pip install requests
+#import requests
