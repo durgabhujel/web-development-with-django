270e3029a332df62fb8a96506c370f663c0b29de
Deepen2056 <deepen916@gmail.com>
2022-01-12 12:26:53 +0545

    oop1.py file

---
+++ python-crash-course/oop/oop1.py
@@ -0,0 +1,13 @@
+#what is oop?
+#oop =Object Oriented progrsmming
+#>OOP is very simpe in python but it is very powerful
+#programming paradigm that uses objects and...... 
+# >classes in programming.
+
+def hello():
+    print("Deepen")
+    print("Bhujel")
+    #return sum
+
+c=hello()
+print(c)
