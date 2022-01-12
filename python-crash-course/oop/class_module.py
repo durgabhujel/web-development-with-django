416cff8e3e7a1e12e9d958f9f64ecd0cb4996ce2
Deepen2056 <deepen916@gmail.com>
2022-01-12 14:00:33 +0545

    added class_module file

---
+++ python-crash-course/oop/class_module.py
@@ -0,0 +1,18 @@
+class car:   # class is a class and car is a class name
+    #attributes and data member  > or variable 
+    a='bmw'
+    b='hundai'
+    c='tesla'
+
+#member function or methods  >functions
+def bmw(self):
+    print(f"hello im {self.a}")
+
+def hundai(self):
+    print(f"hello im {self.a}")
+
+def tesla(self):
+    print(f"hello im {self.a}")         
+
+ojj1=car()
+obj1.bmw    
