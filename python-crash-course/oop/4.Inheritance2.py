7da25c245c685c6099b41d53be941707d316fb50
Deepen2056 <deepen916@gmail.com>
2022-01-17 14:49:37 +0545

    update4.Inheritance2.py file

--- python-crash-course/oop/4.Inheritance2.py
+++ python-crash-course/oop/4.Inheritance2.py
@@ -1,5 +1,11 @@
 #Inheritance constructor overriding
-#in this case we write constructor in classes
+#constructor # Constructor of the parent class is available in the child class.
+# But if we write constructor in both classes, parent and child class then parent class constructor is not available to the child class.
+#in this case only child class constructor is accessible which means child class constructor is replacing parent class constructor
+#in this case only child class constructor is accessible which means child class class constructor replacing the value of parent class
+# Calling Constructor of the parent class in the child class is called constructor overriding.
+# Constructor with super() => super() is a function that is used to call the constructor of the parent class.
+
 '''
 class father:
     def __init__(self):
@@ -44,13 +50,14 @@ class Employee(person):
         self.salary=salary   #we dont need a self.name and self.idnumber because it is child class of person class
         self.post=post
         # invoking the __init__ of the parent class
-        #person.__init__(self, name, idnumber)
+        #person.__init__(self, name, idnumber)   #it is use for pass the name and argument in child class 
+                                                    #if we write this we don't needto write print(self.name and self.id num) and don,t need to write p1.display()
         # or using super()
-        super().__init__(name, idnumber)
+        #super().__init__(name, idnumber)
 
     def display2(self):
-        #print(self.name)
-        #print(self.idnumber)
+        print(self.name)
+        print(self.idnumber)
         print(self.salary)
         print(self.post)
 
@@ -63,10 +70,10 @@ class Employee(person):
 
 
 p1=Employee("Deepen",123,20000,"python Developer")
-p1.display()   
+#p1.display()   
 p1.display2() 
 c=p1.__str__()
-print(c)
-print(type(c))
+print(c)  #it s print Deepen 123 20000 python developer
+print(type(c)) 
 
 
